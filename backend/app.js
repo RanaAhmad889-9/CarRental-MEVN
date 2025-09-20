dad3c31378c415f375b3db9a2d6db56242a27fdb
RanaAhmad889-9 <ranaahmadali860@gmail.com>
2025-09-20 16:12:26 +0500

    Feat(Backend): install mongoose,express and more and create app.js and route.js

---
+++ backend/app.js
@@ -0,0 +1,36 @@
+const express=require('express')
+const app=express()
+const cors=require('cors')
+const session=require('express-session')
+const mongoose=require('mongoose')
+const PORT=3000;
+const DB_PATH = "mongodb+srv://ranaahmadali860:ranaahmadali860@cluster0.wegqf6w.mongodb.net/test?retryWrites=true&w=majority&appName=Cluster0";
+
+app.use(express.json())
+app.use(express.urlencoded({
+    extended:true
+}))
+
+app.use(cors({
+  origin: 'http://localhost:5173',  // Your frontend origin
+  credentials: true                // Allow cookies/sessions
+}));
+
+app.use(session({
+    secret:'carrental',
+    resave:false,
+    saveUninitialized:false,
+    cookie:{
+        httpOnly:true,
+        secure:false,
+        maxAge:1000*60*60*24
+    }
+}))
+
+mongoose.connect(DB_PATH).then(()=>{
+    app.listen(PORT,()=>{
+        console.log(`Port Running at: http://localhost:${PORT}`)
+    })
+}).catch((error)=>{
+    console.log("Failed to Connect",error)
+})
